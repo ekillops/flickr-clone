4298564a5c777fc660df1632594381c4c9a08ae7
Erik Killops and Steve Burton <erik+steve@>
2017-02-15 10:35:38 -0800

    Initial commit

---
+++ FlickrClone.Tests/FlickrCloneTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using FlickrClone.Models;
+using Xunit;
+
+namespace FlickrClone.Tests
+{
+    public class FlickrCloneTest
+    {
+        [Fact]
+        public void Tests_Run()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
